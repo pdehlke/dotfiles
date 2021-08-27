e734da19d49868508a2a235ea0eb2b65dc16d06a
Nanda Lopes <nandalopes@gmail.com>
2021-08-27 15:24:50 -0300

    feat(fzf): integrate fzf with zsh #7
    - Useful functions to tmuxp and fasd

---
+++ zsh/zzz_fzf.zsh
@@ -0,0 +1,9 @@
+### FZF fuzzy finder
+# fzf/install --no-fish --no-update-rc --completion --xdg
+[ -f "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh ] && \
+  source "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh
+
+# custom fzf helpers
+[ -f "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.sh ] && \
+  source "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.sh
+
