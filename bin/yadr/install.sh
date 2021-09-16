d4545f941b2c6ff8b8bda9de8bbe50733257b7eb
Nanda Lopes <nandalopes@gmail.com>
2021-09-16 09:38:33 -0300

    feat: yadr install with chezmoi

---
+++ bin/yadr/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ ! -d "$HOME/.yadr" ]; then
+    echo "Installing YADR for the first time"
+    git clone --depth=1 https://github.com/nandalopes/dotfiles.git "$HOME/.yadr"
+    cd "$HOME/.yadr"
+    sh "./install.sh"
+else
+    echo "YADR is already installed"
+fi
