61df6101c4a6b3c044dc898d30c82698a58b6407
Nanda Lopes <nandalopes@gmail.com>
2022-06-20 22:52:11 -0300

    feat(fzf): fkill a proccess

--- root/private_dot_config/fzf/fzf.sh
+++ root/private_dot_config/fzf/fzf.sh
@@ -104,4 +104,22 @@ z() {
   fi
 }
 
+
+# ------------------------------------------------------------------------
+# process
+
+# fkill - kill processes - list only the ones you can kill. Modified the earlier script.
+fkill() {
+  local pid
+  if [ "$UID" != "0" ]; then
+    pid=$(ps -f -u $UID | sed 1d | fzf --height=40% -m | awk '{print $2}')
+  else
+    pid=$(ps -ef | sed 1d | fzf --height=40% -m | awk '{print $2}')
+  fi
+
+  if [ "x$pid" != "x" ]; then
+    echo $pid | xargs kill -${1:-9}
+  fi
+}
+
 # vim: ft=sh
