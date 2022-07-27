b10d6a38e86ade5862dc3b58d7ef8162384f03a2
Nanda Lopes <nandalopes@gmail.com>
2022-07-27 20:38:44 -0300

    feat(chezmoi): use sh install script

--- install.sh
+++ install.sh
@@ -1,38 +1,45 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-set -e # -e: exit on error
+# -e: exit on error
+# -u: exit on unset variables
+set -eu
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-if [ ! "$(command -v chezmoi)" ]; then
-  bin_dir="$script_dir/root/bin"
-  chezmoi="$bin_dir/chezmoi"
-  echo "Installing chezmoi to $chezmoi"
-  if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
-  elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+if ! chezmoi="$(command -v chezmoi)"; then
+  bin_dir="${script_dir}/root/bin"
+  chezmoi="${bin_dir}/chezmoi"
+  echo "Installing chezmoi to '${chezmoi}'" >&2
+  if command -v curl >/dev/null; then
+    chezmoi_install_script="$(curl -fsSL https://chezmoi.io/get)"
+  elif command -v wget >/dev/null; then
+    chezmoi_install_script="$(wget -qO- https://chezmoi.io/get)"
   else
     echo "To install chezmoi, you must have curl or wget installed." >&2
     exit 1
   fi
-else
-  chezmoi=chezmoi
+  sh -c "${chezmoi_install_script}" -- -b "${bin_dir}"
+  unset chezmoi_install_script bin_dir
 fi
 
-if [ -z "CODESPACES" ]; then
-  echo "Running chezmoi init"
-  "$chezmoi" init "--source=$script_dir"
-  "$chezmoi" diff --verbose
+if [ -z "${CODESPACES:+false}" ]; then
+  echo "Running interactive"
+  "${chezmoi}" init "--source=${script_dir}"
+  "${chezmoi}" diff --verbose
   read -p 'Apply modifications? (y/n) ' r
-  case $r in
+  case "${r}" in
     y|Y|s|S)
-      "$chezmoi" apply --verbose "--source=$script_dir"
+      set -- apply --verbose "--source=${script_dir}"
       ;;
     *)
+      set -- diff
+      ;;
   esac
 else
-  # exec: replace current process with chezmoi
-  exec "$chezmoi" init --apply --verbose "--source=$script_dir"
+  set -- init --apply --verbose --source="${script_dir}"
 fi
+
+echo "Running 'chezmoi $*'" >&2
+# exec: replace current process with chezmoi
+exec "$chezmoi" "$@"
