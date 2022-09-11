6d3aa1b9ce1ba1fd16232308d046e1b271dc0c1c
Nanda Lopes <nandalopes@gmail.com>
2022-09-11 18:22:55 -0300

    📝 docs(cli): fzf install docs

--- root/zsh/zzz_fzf.zsh
+++ root/zsh/zzz_fzf.zsh
@@ -1,5 +1,5 @@
 ### FZF fuzzy finder
-# fzf/install --no-fish --no-update-rc --completion --xdg
+# fzf/install --xdg --all --no-update-rc --no-fish --no-bash
 [ -f "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh ] && \
   source "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh
 
