fd62f3cbf24af8d75da831b3a5bcdc863ee57250
Pete Ehlke <pete.ehlke@aboveproperty.com>
2022-10-08 14:43:50 -0700

    chore(adHoc): Set up vim/sublime the chezmoi way

--- root/bin/yadr/install.sh
+++ root/bin/yadr/install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 if [ ! -d "$HOME/.yadr" ]; then
-    echo "Installing YADR for the first time"
-    git clone --depth=1 https://github.com/nandalopes/dotfiles.git "$HOME/.yadr"
-    cd "$HOME/.yadr"
-    sh "./install.sh"
+  echo "Installing YADR for the first time"
+  git clone --depth=1 https://github.com/pdehlke/dotfiles.git "$HOME/.yadr"
+  cd "$HOME/.yadr"
+  sh "./install.sh"
 else
-    echo "YADR is already installed"
+  echo "YADR is already installed"
 fi
