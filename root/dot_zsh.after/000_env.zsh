38aeda2518f62eedbba61118ba10a9f780f0135e
Pete Ehlke <pete.ehlke@aboveproperty.com>
2022-10-10 09:39:31 -0700

    chore(adhoc): set defaults for version managers

--- root/dot_zsh.after/000_env.zsh
+++ root/dot_zsh.after/000_env.zsh
@@ -2,7 +2,9 @@ export AWS_SESSION_TOKEN_TTL=8h
 export ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future
 export TODOTXT_DEFAULT_ACTION=ls
 
-export JAVA_HOME="$(/usr/libexec/java_home -v 1.8)"
+# DO NOT SET JAVA_HOME. jenv will manage that for you.
+# See the comments in 01_version_managers.zsh
+# export JAVA_HOME="$(/usr/libexec/java_home -v 1.8)"
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export HOMEBREW_NO_AUTO_UPDATE=1
 #export HOMEBREW_AUTO_UPDATE_SECS=604800
--- root/dot_zsh.after/01_version_managers.zsh
+++ root/dot_zsh.after/01_version_managers.zsh
@@ -6,20 +6,44 @@ if quiet_which direnv; then
   eval "$(direnv hook zsh)"
 fi
 
+# Our ember projects are pinned to node 12.
+# We've found that 12.22.12 is mostly viable
+# across developers' systems.
+#
+# "The node community made it clear that they value
+# expressivity and velocity over robustness and predictability."
+# -Bryan Cantrill
+#
 if [[ -f $(brew --prefix nvm)/nvm.sh ]]; then
   export NVM_DIR=${HOME}/.nvm
   source $(brew --prefix nvm)/nvm.sh
+  if [[ $(node -v) != "v12.22.12" ]]; then
+    nvm install v12.22.12 >/dev/null 2>&1
+    nvm alias default v12.22.12 >/dev/null 2>&1
+  fi
 fi
 
+# Install JDKs/JVMs via homebrew please. They end up
+# in the right place and jenv will handle it all.
+# Use 1.8 by default
+#
 if quiet_which jenv; then
   eval "$(jenv init -)"
-  jenv enable-plugin export
+  jenv enable-plugin export >/dev/null 2>&1
+  for i in /Library/Java/JavaVirtualMachines/*; do
+    jenv add $i/Contents/Home >/dev/null 2>&1
+  done
+  jenv global 1.8
 fi
 
+# We don't use much ruby, but if you do, here's your huckleberry.
+#
 if quiet_which rbenv; then
   eval "$(rbenv init -)"
 fi
 
+# golang had so much promise...
+#
 if quiet_which goenv; then
   export GOENV_ROOT="$HOME/.goenv"
   export PATH="$GOENV_ROOT/bin:$PATH"
