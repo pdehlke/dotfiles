db645274cdb42c6f1564413451f61f825fdbd772
Nanda Lopes <nandalopes@gmail.com>
2023-06-02 11:50:41 -0300

    style(cli): fzf exclude shells list

--- root/zsh/zzz_fzf.zsh
+++ root/zsh/zzz_fzf.zsh
@@ -1,5 +1,6 @@
 ### FZF fuzzy finder
-# fzf/install --xdg --all --no-update-rc --no-fish --no-bash
+# fzf/install --xdg --all --no-update-rc \
+#   --no-fish --no-bash
 [ -f "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh ] && \
   source "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh
 
