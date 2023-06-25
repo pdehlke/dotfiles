9cb35bc23ed6db5adb5bafe4e72bc53726e594e4
Nanda Lopes <nandalopes@gmail.com>
2023-06-25 13:22:10 -0300

    refactor(cli): asdf zsh loading

--- root/zsh/asdf.zsh
+++ root/zsh/asdf.zsh
@@ -1,14 +1,19 @@
 ### ASDF Config
 if [ -d "${ASDF_DIR:-$HOME/.asdf}" ]; then
   ASDF_DIR="${ASDF_DIR:-$HOME/.asdf}"
-  ASDF_DATA_DIR="${ASDF_DATA_DIR:-$HOME/.asdf}"
+  ASDF_DATA_DIR="${ASDF_DATA_DIR:-$ASDF_DIR}"
   export ASDF_DIR ASDF_DATA_DIR
 
   # Don't source `$ASDF_DIR/asdf.sh`
-  #source $ASDF_DIR/asdf.sh
+  if [ -f "$ASDF_DIR/lib/asdf.sh" ]; then
+    source $ASDF_DIR/lib/asdf.sh
+  fi
 
   # prepend $ASDF_DIR/bin to PATH
-  PATH="$ASDF_DIR/bin:$PATH"
+  path=(
+    $ASDF_DIR/bin
+    $path
+  )
 
   ### append zsh completions
   fpath=(
