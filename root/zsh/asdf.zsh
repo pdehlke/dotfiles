6b9d35af3a18bc5ddcc035990d62e318ae01f98e
Nanda Lopes <nandalopes@gmail.com>
2025-02-15 15:19:05 -0300

    fix(cli): don't add `$ASDF_DATA_DIR/bin` in path

--- root/zsh/asdf.zsh
+++ root/zsh/asdf.zsh
@@ -3,23 +3,6 @@ if [ -d "${ASDF_DIR:-$HOME/.asdf}" ]; then
   ASDF_DIR="${ASDF_DIR:-$HOME/.asdf}"
   ASDF_DATA_DIR="${ASDF_DATA_DIR:-$ASDF_DIR}"
   export ASDF_DIR ASDF_DATA_DIR
-
-  # Don't source `$ASDF_DIR/asdf.sh`
-  if [ -f "$ASDF_DIR/lib/asdf.sh" ]; then
-    source $ASDF_DIR/lib/asdf.sh
-  fi
-
-  # prepend $ASDF_DIR/bin to PATH
-  path=(
-    $ASDF_DIR/bin
-    $path
-  )
-
-  ### append zsh completions
-  fpath=(
-    $fpath
-    $ASDF_DIR/completions
-  )
 fi
 
 ##### Direnv
