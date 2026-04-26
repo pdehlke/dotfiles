9b8c4bd302f712058a889a8511a8beb852177232
Pete Ehlke <pde@rfc822.net>
2026-04-26 14:52:11 -0700

    chore: add a few small LazyVim options

--- root/private_dot_config/nvim/lua/config/options.lua
+++ root/private_dot_config/nvim/lua/config/options.lua
@@ -1,4 +1,18 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- I like line wrap. Sue me.
+vim.wo.wrap = true
+
+-- Set the full path and status in the winbar
 vim.opt.winbar = "%=%m %f"
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Save undo history
+vim.opt.undofile = true
