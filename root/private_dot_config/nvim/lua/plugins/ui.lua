b0ece74e871eacd78eb8c2ae8157f4fdf2ab11a3
Pete Ehlke <pde@rfc822.net>
2026-05-14 10:30:29 -0700

    feat: better approach to nvim/direnv

--- root/private_dot_config/nvim/lua/plugins/ui.lua
+++ root/private_dot_config/nvim/lua/plugins/ui.lua
@@ -120,32 +120,6 @@ return {
                 return mutagen[cwd]
             end
 
-            ---@type table<string, {updated:number, allowed: boolean}>
-            local direnv = {}
-
-            local function direnv_status()
-                if vim.fn.executable("direnv") == 0 then
-                    return nil
-                end
-
-                local direnv_file = vim.fs.find(".envrc", { path = vim.uv.cwd() or ".", upward = true })[1]
-                if not direnv_file then
-                    return nil
-                end
-
-                local cwd = vim.uv.cwd() or "."
-                direnv[cwd] = direnv[cwd] or { updated = 0, allowed = false }
-
-                local now = vim.uv.now() -- timestamp in milliseconds
-                if direnv[cwd].updated + 5000 < now then
-                    local output = vim.fn.system("direnv status --json | jq -e '.state.loadedRC.allowed == 0'")
-                    direnv[cwd].allowed = vim.v.shell_error == 0
-                    direnv[cwd].updated = now
-                end
-
-                return direnv[cwd]
-            end
-
             local error_color = { fg = Snacks.util.color("DiagnosticError") }
             local ok_color = { fg = Snacks.util.color("DiagnosticInfo") }
             table.insert(opts.sections.lualine_x, {
@@ -166,37 +140,13 @@ return {
             })
 
             table.insert(opts.sections.lualine_x, {
-                cond = function()
-                    return direnv_status() ~= nil
-                end,
-                color = function()
-                    local status = direnv_status()
-                    return status and status.allowed and ok_color or error_color
-                end,
                 function()
-                    local status = direnv_status()
-                    if not status then
-                        return ""
-                    end
-                    return (status.allowed and "󱥒 " or "󱥒 ") .. "direnv"
+                    return require("direnv").statusline()
                 end,
+                "encoding",
+                "fileformat",
+                "filetype",
             })
         end,
     },
-
-    -- "folke/twilight.nvim",
-
-    -- {
-    --   "folke/zen-mode.nvim",
-    --   cmd = "ZenMode",
-    --   opts = {
-    --     window = { backdrop = 0.7 },
-    --     plugins = {
-    --       gitsigns = true,
-    --       tmux = true,
-    --       kitty = { enabled = false, font = "+2" },
-    --     },
-    --   },
-    --   keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
-    -- },
 }
