4f650a9d121b73d8dfbea5747643b5d05507af97
Pete Ehlke <pde@rfc822.net>
2026-06-22 12:07:16 -0700

    chore: ensure signed commits

--- root/dot_zsh.after/000_env.zsh
+++ root/dot_zsh.after/000_env.zsh
@@ -9,6 +9,7 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export HOMEBREW_NO_AUTO_UPDATE=1
 #export HOMEBREW_AUTO_UPDATE_SECS=604800
 export HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_NO_INSECURE_REDIRECT=1
 export BAT_THEME="Solarized (dark)"
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#000000,bg=cyan,underline"
